71ba8edcc1404d8e4bb1326965916fc8ea5cfc01
senbe0 <sambei13579@gmail.com>
2023-11-07 00:35:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import * as THREE from "three";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
 
-function App() {
+
+import { ImageList } from './components/ImageList';
+
+export default function App() {
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+    let container, stats;
+    let camera, scene, renderer;
+    let controls;
+    let animateId;
+
+    // containerをrefから取得
+    container = mountRef.current;
+    
+    // 初期化とアニメーション
+    init();
+    animate();
+
+    // ウィンドウリサイズイベントリスナーを設定
+    window.addEventListener('resize', onWindowResize);
+    onWindowResize(); // 初期リサイズトリガー
+
+    // クリーンアップ関数
+    return () => {
+      window.removeEventListener('resize', onWindowResize);
+      cancelAnimationFrame(animateId);
+      container.removeChild(renderer.domElement);
+    };
+
+    function init() {
+      renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.outputEncoding = THREE.sRGBEncoding;
+      renderer.shadowMap.enabled = true;
+      container.appendChild(renderer.domElement);
+
+      camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 2, 100);
+      camera.position.set(0, 0, 15);
+
+      controls = new TrackballControls(camera, renderer.domElement);
+
+      scene = new THREE.Scene();
+      scene.background = new THREE.Color(0x999999);
+
+      var loader = new PLYLoader();
+      loader.load("models/brain_3d.ply", function(geometry) {
+        geometry.computeVertexNormals();
+        geometry.computeBoundingBox();
+
+        var centroid = new THREE.Vector3();
+        geometry.boundingBox.getCenter(centroid);
+        geometry.translate(-centroid.x, -centroid.y, -centroid.z);
+
+        var material = new THREE.PointsMaterial({
+          size: 0.02,
+          vertexColors: true
+        });
+
+        var points = new THREE.Points(geometry, material);
+        points.scale.multiplyScalar(0.005);
+        scene.add(points);
+      });
+
+      var axesHelper = new THREE.AxesHelper(5);
+      scene.add(axesHelper);
+
+      window.addEventListener("resize", onWindowResize, false);
+    }
+
+    function onWindowResize() {
+      const width = window.innerWidth * 0.5;
+      const height = window.innerHeight * 0.7;
+
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      renderer.setSize(width, height);
+    }
+
+    function animate() {
+      animateId = requestAnimationFrame(animate);
+      controls.update();
+      render();
+      
+    }
+
+    function render() {
+      renderer.render(scene, camera);
+    }
+  }, []);
+
+  // 親コンテナのスタイルを定義
+  const bodyStyle = {
+    display: 'flex',
+    // flexDirection: 'row',
+    // width: '100%',
+    // height: '100vh',
+    // margin: 0,
+    // padding: 0,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={bodyStyle}>
+      <ImageList />
+      {/* <div style={{ flexBasis: '50%' }}>helloooooooooooooooo<br/>Sg</div> */}
+      <div ref={mountRef} />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/ImageList.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+// スタイルを定義します。必要に応じてCSSファイルに移動しても構いません。
+const styles = {
+  container: {
+    width: '50vw', // コンテナの幅をビューポートの幅の50%にします。
+    margin: '0 auto', // コンテナを画面の中央に配置します。
+    border: '1px solid #ccc', // 見た目を整えるためのボーダーです。
+    padding: '20px', // 内容とボーダーの間にスペースを確保します。
+    boxSizing: 'border-box', // パディングとボーダーを幅に含めます。
+  },
+  listContainer: {
+    maxHeight: '20vh', // リストの最大高さをビューポートの高さの50%にします。
+    overflowY: 'scroll', // 縦スクロールを可能にします。
+    marginBottom: '20px', // 画像とリストの間にスペースを追加します。
+  },
+  imageContainer: {
+    display: 'flex', // Flexbox コンテナとして設定
+    justifyContent: 'space-around', // 画像間に均等なスペースを設定
+  },
+  image: {
+    maxWidth: '50%', // 画像の最大幅をコンテナの幅の50%に設定
+    height: 'auto', // 画像の高さを自動調整
+    display: 'block', // ブロックレベル要素として画像を表示
+    marginBottom: '10px', // 次の画像との間にスペースを追加
+  },
+  button: {
+    display: 'block', // ボタンをブロック要素として表示します。
+    width: '100%', // ボタンの幅をリストの幅に合わせます。
+    marginBottom: '5px', // ボタン同士の間にスペースを追加します。
+    padding: '10px', // ボタン内のスペースを確保します。
+    boxSizing: 'border-box', // パディングを幅に含めます。
+  }
+};
+
+export const ImageList = () => {
+  // 選択された画像とそのマスクを状態として持つ
+  const [selectedImage, setSelectedImage] = useState({
+    image: 'TCGA_HT_7881_19981015_1.png',
+    mask: 'TCGA_HT_7881_19981015_1_mask.png' // 初期マスク画像
+  });
+
+  const images = Array.from({ length: 80 }, (_, index) => ({
+    image: `TCGA_HT_7881_19981015_${index + 1}.png`,
+    mask: `TCGA_HT_7881_19981015_${index + 1}_mask.png` // 各画像に対応するマスクのパス
+  }));
+
+  const handleClick = (imageObj) => {
+    setSelectedImage(imageObj);
+  };
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.listContainer}>
+        {images.map((imageObj, index) => (
+          <button 
+            key={index} 
+            onClick={() => handleClick(imageObj)} 
+            style={styles.button}
+          >
+            {imageObj.image}
+          </button>
+        ))}
+      </div>
+      <div style={styles.imageContainer}>
+        <img 
+          src={`images/TCGA_HT_7881_19981015_png/${selectedImage.image}`} 
+          alt={`Image ${selectedImage.image}`} 
+          style={styles.image}
+        />
+        <img 
+          src={`images/TCGA_HT_7881_19981015_png/${selectedImage.mask}`} 
+          alt={`Mask ${selectedImage.mask}`} 
+          style={styles.image}
+        />
+      </div>
+    </div>
+  );
+};
